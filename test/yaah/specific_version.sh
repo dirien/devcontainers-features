eebb0be4746f914c2e66e6e9dc220e42b56095c0
Engin Diri <engin.diri@ediri.de>
2026-03-21 15:25:26 +0100

    Bump yaah feature to v0.2.0
    Update default example version and test scenarios to match
    the latest yaah release.

--- test/yaah/specific_version.sh
+++ test/yaah/specific_version.sh
@@ -4,6 +4,6 @@ set -e
 source dev-container-features-test-lib
 
 check "yaah is installed" which yaah
-check "yaah version is 0.1.8" bash -c "yaah version | grep '0.1.8'"
+check "yaah version is 0.2.0" bash -c "yaah version | grep '0.2.0'"
 
 reportResults
