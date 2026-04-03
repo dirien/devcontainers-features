156d67f35f8e39893af8927d985cc0aaeb806be3
Engin Diri <engin.diri@ediri.de>
2026-04-03 11:10:18 +0200

    chore: remove yaah feature (moved to dirien/devcontainer-feature-yaah)

--- test/yaah/specific_version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-source dev-container-features-test-lib
-
-check "yaah is installed" which yaah
-check "yaah version is 0.2.0" bash -c "yaah version | grep '0.2.0'"
-
-reportResults
